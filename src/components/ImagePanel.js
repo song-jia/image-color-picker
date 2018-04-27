77f302ff0ebc3801b3c21e38308c93e96eb10708
song-jia <songjia.happycoding@gmail.com>
2018-04-27 22:00:41 +0800

    using canvas display image.

--- src/components/ImagePanel.js
+++ src/components/ImagePanel.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import DropZone from 'react-dropzone';
 
 class ImagePanel extends Component {
@@ -36,7 +37,7 @@ class ImagePanel extends Component {
             >
 
                 {this.state.previewImage
-                    ? <PreviewCanvas image={this.state.previewImage} />
+                    ? <PreviewCanvas imageSrc={this.state.previewImage} />
                     : <ChooseFile onClick={this.chooseFile} />}
             </DropZone>
         );
@@ -53,10 +54,73 @@ function ChooseFile({ onClick }) {
     );
 }
 
-function PreviewCanvas({ image }) {
-    return (
-        <img src={image} alt="" />
-    );
+class PreviewCanvas extends React.Component {
+    constructor(props) {
+        super(props);
+        this.anvas = null;
+    }
+
+    componentDidMount() {
+        this.drawImage();
+    }
+
+    componentDidUpdate() {
+        this.drawImage();
+    }
+
+    drawImage() {
+        if (!this.props.imageSrc) {
+            return;
+        }
+
+        const image = new Image();
+
+        image.onload = () => {
+            const ctx = this.canvas.getContext('2d');
+
+            // ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width, image.height);
+            ctx.drawImage(image, ...centerImage(this.canvas, image));
+        };
+
+        image.src = this.props.imageSrc;
+
+        function centerImage(canvas, image) {
+            let x = 0,
+                y = 0,
+                w = 0,
+                h = 0;
+
+            if (image.width >= image.height) {
+                w = canvas.width < image.width ? canvas.width : image.width;
+                h = w / image.width * image.height;
+            } else {
+                h = canvas.height < image.height ? canvas.height : image.height;
+                w = h / image.height * image.width;
+            }
+
+            x = (canvas.width - w) / 2;
+            y = (canvas.height - h) / 2;
+
+            return [x, y, w, h];
+        }
+    }
+
+    render() {
+        return (
+            <canvas className="preview-canvas" ref={(ref) => {
+                if (ref) {
+                    ref.width = ref.clientWidth;
+                    ref.height = ref.clientHeight;
+                }
+
+                this.canvas = ref;
+            }} />
+        );
+    }
+}
+
+PreviewCanvas.propTypes = {
+    imageSrc: PropTypes.string
 }
 
 export default ImagePanel;
